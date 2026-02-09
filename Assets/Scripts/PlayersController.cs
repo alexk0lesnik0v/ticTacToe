442cb1a824bb1546d9ddfb706d93d614709035b2
alexk0lesnik0v <alexander74k@mail.ru>
2026-02-09 06:42:42 +0300

    Add condition of win and add MenuCanvas and WinCanvas

--- Assets/Scripts/PlayersController.cs
+++ Assets/Scripts/PlayersController.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayersController : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> m_slots;
+    [SerializeField] private List<Slot> m_slots;
+    [SerializeField] private TMP_Text m_playersTurnText;
     
     private int m_index = 0;
 
@@ -11,15 +13,66 @@ public class PlayersController : MonoBehaviour
     {
         m_index = 0;
     }
-
+   
     public int CrossOrZero()
     {
         m_index++;
+
+        if (m_index % 2 == 0)
+        {
+            m_playersTurnText.text = "PLAYER 1 TURN";
+        }
+        else
+        {
+            m_playersTurnText.text = "PLAYER 2 TURN";
+        }
+        
         return m_index;
     }
-
-    private void Update()
+    
+    public void CheckWin()
     {
+        if (m_slots[4].sprite !=null)
+        {
+            if (m_slots[4].sprite == m_slots[0].sprite && m_slots[4].sprite == m_slots[8].sprite
+                || m_slots[4].sprite == m_slots[1].sprite && m_slots[4].sprite == m_slots[7].sprite
+                || m_slots[4].sprite == m_slots[2].sprite && m_slots[4].sprite == m_slots[6].sprite
+                || m_slots[4].sprite == m_slots[3].sprite && m_slots[4].sprite == m_slots[5].sprite)
+            {
+               CheckWinner();
+            }
+        }
         
+        if (m_slots[0].sprite !=null)
+        {
+            if (m_slots[0].sprite == m_slots[1].sprite && m_slots[0].sprite == m_slots[2].sprite
+                || m_slots[0].sprite == m_slots[3].sprite && m_slots[0].sprite == m_slots[6].sprite)
+            {
+                CheckWinner();
+            }
+        }
+        
+        if (m_slots[8].sprite !=null)
+        {
+            if (m_slots[8].sprite == m_slots[2].sprite && m_slots[8].sprite == m_slots[5].sprite
+                || m_slots[8].sprite == m_slots[6].sprite && m_slots[8].sprite == m_slots[7].sprite)
+            {
+                CheckWinner();
+            }
+        }
+    }
+
+    private void CheckWinner()
+    {
+        if (m_index % 2 == 0)
+        {
+            Debug.Log("Player 2 Win!!!");
+            m_playersTurnText.text = "";
+        }
+        else
+        {
+            Debug.Log("Player 1 Win!!!");
+            m_playersTurnText.text = "";
+        }
     }
 }
--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -13,6 +13,8 @@ public class Slot : MonoBehaviour, IPointerDownHandler
     [SerializeField] AudioSource m_audioSource;
     [SerializeField] private AudioClip m_audioClip;
 
+    public Sprite sprite = null;
+
     private void Start()
     {
         m_playerController = FindObjectOfType<PlayersController>();
@@ -27,12 +29,18 @@ public class Slot : MonoBehaviour, IPointerDownHandler
         if (index % 2 == 0)
         {
             m_image.GetComponent<Image>().sprite = m_zero;
+            sprite = m_zero;
             m_audioSource.PlayOneShot(m_audioClip);
+            
+            m_playerController.CheckWin();
         }
         else
         {
             m_image.GetComponent<Image>().sprite = m_cross;
+            sprite = m_cross;
             m_audioSource.PlayOneShot(m_audioClip);
+            
+            m_playerController.CheckWin();
         }
     }
 }
