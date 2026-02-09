6ffad5b97ac24022a4212dc74c1e5e727e3f0fac
alexk0lesnik0v <alexander74k@mail.ru>
2026-02-09 04:40:38 +0300

    Create simple mechanics of playing

---
+++ Assets/Scripts/PlayersController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersController : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> m_slots;
+    
+    private int m_index = 0;
+
+    private void Start()
+    {
+        m_index = 0;
+    }
+
+    public int CrossOrZero()
+    {
+        m_index++;
+        return m_index;
+    }
+
+    private void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Slot : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField] private PlayersController m_playerController;
+    [SerializeField] private GameObject m_image;
+    
+    [SerializeField] private Sprite m_cross;
+    [SerializeField] private Sprite m_zero;
+    
+    [SerializeField] AudioSource m_audioSource;
+    [SerializeField] private AudioClip m_audioClip;
+
+    private void Start()
+    {
+        m_playerController = FindObjectOfType<PlayersController>();
+        m_image = transform.GetChild(0).gameObject;
+        m_audioSource = GetComponent<AudioSource>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        int index = m_playerController.CrossOrZero();
+        
+        if (index % 2 == 0)
+        {
+            m_image.GetComponent<Image>().sprite = m_zero;
+            m_audioSource.PlayOneShot(m_audioClip);
+        }
+        else
+        {
+            m_image.GetComponent<Image>().sprite = m_cross;
+            m_audioSource.PlayOneShot(m_audioClip);
+        }
+    }
+}
